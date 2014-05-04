7c481410f7fb46b9da5da2f88c24a2e659778b8b
Alek Slater <slayte@gmail.com>
2014-05-04 17:36:18 +0800

    added some more arguments along with help. added support for config files

--- itunes-helper.py
+++ itunes-helper.py
@@ -1,24 +1,42 @@
 
 from pyqtmeta.meta import Helper
 import os
-import sys
+import json
 
-if not len(sys.argv) == 2:
-    exit("Missing filename parameter. Syntax should be:\nitunes-helper.py file-name")
-else:
-    t = sys.argv[1]
 
-movie_path = t
+class Config(object):
+    
+    def __init__(self, d):
+        self.tv_folder = None if not "tv_folder" in d else d["tv_folder"]
+        self.movie_folder = None if not "movie_folder" in d else d["movie_folder"]
 
-# with open(movie_path, 'rb') as fp:
-#     movie = fp.read()
-#     movie = open(movie_path, 'rb').read()
-#     movie_atoms = AtomTree(movie)
-#     meta_atom = movie_atoms.getAtomByPath(AtomPaths.meta)
-#     show_atom = movie_atoms.getAtomByPath(AtomPaths.video_episode)
-#     print movie_atoms.printTree()
-#     print meta_atom.data
-#     print show_atom
+    @staticmethod
+    def from_config_file(path_to_config_file):
+        if os.path.exists(path_to_config_file):
+            with open(path_to_config_file, "r") as fp:
+                s = fp.read()
+                d = json.loads(s)
+                return Config(d)
+        return None
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file_name", help="the path to the video file")
+parser.add_argument("-c", "--config", help="the path to the config file (if none supplied it will expect it at ~/.itunes-helper.conf")
+args = parser.parse_args()
+
+movie_path = args.file_name
+config_path = args.config
+
+if config_path is None:
+    config_path = os.path.expanduser("~/.itunes-helper.conf")
+config = Config.from_config_file(config_path)
+
+if config is None:
+    d1 = {"tv_folder": "~/Movies/Series", "movie_folder": "~/Movies"}
+    d2 = {"tv_folder": "/Volumes/Terra/movies/series", "movie_folder": "/Volumes/Terra/movies"}
+    exit("Config is missing, a sample .itunes-helper.conf looks like this:\n%s\nor\n%s" % (str(d1), str(d2)))
 
 h = Helper()
 
@@ -28,10 +46,10 @@ destination_folder = None
 meta_data = None
 if i == '0':
     meta_data = h.infer_metadata_from_tvshow_file(movie_path)
-    destination_folder = "/Volumes/Terra/movies/series"
+    destination_folder = config.tv_folder
 elif i == '1':
     meta_data = h.infer_metadata_from_movie_file(movie_path)
-    destination_folder = "/Volumes/Terra/movies"
+    destination_folder = config.movie_folder
 
 if meta_data is None or len(meta_data) == 0:
     exit("Found no meta data")
